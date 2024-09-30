901c79e23d8dc213c97a8b63851a3190a732a037
Wladek <wladek.developer@gmail.com>
2024-09-30 12:17:49 +0200

    refactor: change code style of Gradient Section

--- src/components/GradientSection/GradientSection.tsx
+++ src/components/GradientSection/GradientSection.tsx
@@ -10,12 +10,12 @@ import style from "./GradientSection.module.css"
 export function GradientSection () {
 
   const requestAnimationFrameRef = useRef(0)
-  const [curPos, setCurPos] = useState({ x: 0, y: 0 })
+  const [cursorPos, setCursorPos] = useState({ x: 0, y: 0 })
   const mousePosition = useMousePosition()
 
 
   function moveCursorBlob (): void {
-    setCurPos(prev => ({
+    setCursorPos(prev => ({
       x: Math.round(prev.x + (mousePosition.x - prev.x) / 20),
       y: Math.round(prev.y + (mousePosition.y - prev.y) / 20)
     }))
@@ -30,24 +30,43 @@ export function GradientSection () {
 
   return(
     <section className={style.gradient}>
+
       <h1>HELLO WORLD</h1>
+
       <svg>
         <defs>
-          <filter id="svgFilter1">
-            <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
-            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8" result="svgFilter1" />
-            <feBlend in="SourceGraphic" in2="svgFilter1" />
+          <filter id="gradientSectionSvgFilter">
+            <feGaussianBlur
+              in="SourceGraphic"
+              stdDeviation="10"
+              result="blur"
+            />
+            <feColorMatrix
+              in="blur"
+              mode="matrix"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
+              result="gradientSectionSvgFilter"
+            />
+            <feBlend
+              in="SourceGraphic"
+              in2="gradientSectionSvgFilter"
+            />
           </filter>
         </defs>
       </svg>
+
       <div className={style.blobsContainer}>
-        <div className={style.blob1}></div>
-        <div className={style.blob2}></div>
-        <div className={style.blob3}></div>
-        <div className={style.blob4}></div>
-        <div className={style.blob5}></div>
-        <div className={style.cursorBlob} style={{ top:`calc(-40% + ${curPos.y}px)`, left:`calc(-40% + ${curPos.x}px)` }}></div>
+        <div className={style.blob1} />
+        <div className={style.blob2} />
+        <div className={style.blob3} />
+        <div className={style.blob4} />
+        <div className={style.blob5} />
+        <div
+          className={style.cursorBlob}
+          style={{ top:`calc(-40% + ${cursorPos.y}px)`, left:`calc(-40% + ${cursorPos.x}px)` }}
+        />
       </div>
+
     </section>
   )
 }
